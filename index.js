704fb0ed43f215957734bce5ef2cfa85e17ae8af
Victor <GGVictor7373@outlook.com>
2026-01-28 03:29:01 +0100

    feat: implement PostgreSQL integration and add user listing endpoint

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const exspress = require('express');
 
 const app = exspress();
 
+const {  Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'mysecretpassword',
+    port: 5432,
+});
+
 app.get(
     '/', (req, res) => {
-        res.send(`
-                    <h1>Hello World</h1>
-                    <p>This is a server-side rendered page using Node.js and Express.</p> 
-                `)
+        res.json({ message: 'Hello, World!' });
 });
+
+app.get('/deltagere2', async (req, res) => {
+
+    const result = await pool.query('SELECT * FROM users');
+
+    let html = '<h1>List of Users</h1><ul>';
+    html += "<ul>"
+
+    for( const row of result.rows ) {
+        html += `<li>${row.name}</li>`;
+    }
+    html += "</ul>";
+
+    res.send(html);
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
