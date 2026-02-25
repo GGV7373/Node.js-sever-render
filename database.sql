bb9dc6cd881aa382d11a5e1064cf4d70f5c4f2ab
Victor <GGVictor7373@outlook.com>
2026-02-25 10:44:35 +0100

    feat: update docker-compose service name and add deltagere.html for user interaction

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+-- Active: 1772011816664@@localhost@5432@postgres@public
+-- Active: 1772011816664@@localhost@5432@postgres
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL
